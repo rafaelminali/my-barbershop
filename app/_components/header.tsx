d7632d3630b7a62fedfb8c7dec6381f78115445f
rafaelminali <rafael-minali@hotmail.com>
2024-02-04 19:00:35 -0300

    feat: add auth provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+
 
 const Header = () => {
     return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="ptBR">
       <body className={`${inter.className} dark`}>{children}</body>
+      <AuthProvider>
+        {children}
+      </AuthProvider>
     </html>
   );
 }
