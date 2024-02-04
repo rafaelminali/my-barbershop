0b858a9e1b676f0e4034b487eaecfa17c134b1e2
rafaelminali <rafael-minali@hotmail.com>
2024-02-04 02:09:26 -0300

    feat: add initial Footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import Footer from "../_components/footer";
 
 export default async function Home() {
 
@@ -37,6 +38,15 @@ export default async function Home() {
         </div>
       </div>
 
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">Populares</h2>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
